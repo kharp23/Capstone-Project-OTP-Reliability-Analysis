f6c0e749ebaa3fccac17757f14a5c29572b5941a
kharp23 <kaurharpreet23@icloud.com>
2026-07-06 14:41:15 -0400

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,21 @@
+import pandas as pd
 
+files = [
+    r"file1path.csv",
+    r"file2path.csv",
+    r"file3path.csv",
+    r"file4path.csv",
+    r"file5path.csv"
+]
+
+dfs = [pd.read_csv(file) for file in files]
+
+OTP_df = pd.concat(dfs, ignore_index=True)
+
+print("Merged Dataset Shape:", OTP_df.shape)
+
+OTP_df.head()
+
+OTP_df.to_csv("OTP_df.csv", index=False)
+
+print("Merged file saved as merged_dataset.csv")
